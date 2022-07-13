c3590050f15eca5ddcc444000a2956b5c9fbf910
sec248 <sec248@proton.me>
2022-07-13 23:14:47 +0300

    add cli parser

--- Asudo/Main.asm
+++ Asudo/Main.asm
@@ -7,16 +7,42 @@ include C:\masm32\include\user32.inc
 includelib C:\masm32\include\user32.lib
 
 .data
-    hTitle db 'Win32', 0
-    hMsg db 'Hello World', 0
+    hTitle db "Hello", 0
+    executableName db 1024 dup(0)
 
 .code
     Main:
+        ; Get Module File Name Here
+        push 1024
+        push offset executableName
+        push 0
+        call GetModuleFileNameA
+
+        ; Increase the Returned Length To Ignore Double Quotes
+        inc eax
+        inc eax
+
+        ; Save to the EBX
+        mov ebx, eax
+
+        ; Get Full CLI Arguments
+        call GetCommandLine
+
+        ; EAX = CLI | EBX = FIRST ARGUMENT LENGTH
+        ; ECX = CLI + LENGTH
+        ; For Example, ["C:\thing.exe" aaa bbb] -> [aaa bbb]
+        lea ecx, [eax + ebx]
+
+        ; A Code for Debug
         push 0
         push offset hTitle
-        push offset hMsg
+        push ecx
         push 0
         call MessageBoxA
-        push eax
-        call ExitProcess
+
+        jmp Exit
+
+        Exit:
+            push 0
+            call ExitProcess
     End Main
